17552b9170893dc0de41bae826c5afa9c8d98994
Luka-Gatkuoth <lukagatkuoth4@gmail.com>
2024-07-03 17:13:15 -0700

    puppet file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#creating file in/tmp using puppet
+
+file {"Gatkuoth's puppet":
+  path    => 'temp/school',
+  mode    => '0744',
+  0wner   => 'www-data',
+  group   => 'www-data',
+  content => 'i love puppet'
+}
